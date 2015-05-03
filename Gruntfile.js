63119a8acc2677f8eae989c8217698a6ceeb38ea
J. Harshbarger <hypercubed@gmail.com>
2015-05-03 20:56:34 +0900

    Added gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -386,6 +386,13 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'dist'
+      },
+      src: ['**']
     }
   });
 
